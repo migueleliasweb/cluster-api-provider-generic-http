5a69b326bc2d7d8605946b04a57c4e94eac3e2c2
Miguel Elias dos Santos <migueleliasweb@gmail.com>
2021-12-28 20:42:14 +1100

    Use golang upstream image

--- hack/run.sh
+++ hack/run.sh
@@ -6,11 +6,13 @@ if [ -z "${ENTRYPOINT}" ]; then
 fi
 
 if [ -z "${CMD}" ]; then
-    echo "The variable 'CMD' is required."
-    exit 1
+    docker run -it --rm \
+        -v ${PWD}:/opt/cluster-api-provider-generic-http \
+        --entrypoint "${ENTRYPOINT}" \
+        cluster-api-provider-generic-http
+else
+    docker run -it --rm \
+        -v ${PWD}:/opt/cluster-api-provider-generic-http \
+        --entrypoint "${ENTRYPOINT}" \
+        cluster-api-provider-generic-http "${CMD}"
 fi
-
-docker run -it --rm \
-    -v ${PWD}:. \
-    --entrypoint "${ENTRYPOINT}" \
-    cluster-api-provider-generic-http "${CMD}"
